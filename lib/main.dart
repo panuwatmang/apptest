85146947a60d2388c0853b0d638ce4e9950e5e20
Panuwat Noonkong <107528039+panuwatmang@users.noreply.github.com>
2024-03-19 10:28:31 +0700

    AppController

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ruts/states/authen.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Authen(),);
+    return GetMaterialApp(home: Authen(),);
   }
 }
--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ruts/utility/app_constant.dart';
+import 'package:flutter_ruts/utility/app_controller.dart';
 import 'package:flutter_ruts/widgets/widget_form.dart';
 import 'package:flutter_ruts/widgets/widget_icon_button.dart';
 import 'package:flutter_ruts/widgets/widget_image_assets.dart';
 import 'package:flutter_ruts/widgets/widget_text.dart';
+import 'package:get/get.dart';
 
-class Authen extends StatelessWidget {
+class Authen extends StatefulWidget {
   const Authen({super.key});
+
+  @override
+  State<Authen> createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  //นี่คือ Dependeccy สำหรับ Call ตัวแปลที่อยู่ใน Controller
+  AppController appController = Get.put(AppController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      body: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
         child: ListView(
           children: [
             Column(
@@ -29,15 +40,17 @@ class Authen extends StatelessWidget {
                       hint: 'Username :',
                       subfixWidget: Icon(Icons.android),
                     ),
-                    WidgetForm(
-                      hint: 'Password :',
-                      obscu: true,
-                      subfixWidget: widgetIconButton(
-                        iconData: Icons.remove_red_eye,
-                        pressFunc: () {},
-                      ),
-                      
-                    ),
+                    Obx(() => WidgetForm(
+                          hint: 'Password :',
+                          obscu: appController.redEye.value,
+                          subfixWidget: widgetIconButton(
+                            iconData: Icons.remove_red_eye,
+                            pressFunc: () {
+                              appController.redEye.value =
+                                  !appController.redEye.value;
+                            },
+                          ),  
+                        )),
                   ],
                 ),
               ],
